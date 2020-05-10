876828227bed677f37018835998c1256794a1346
Chacko <chackop@gmail.com>
2020-05-10 16:32:58 +0100

    passing in api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import WrappedComponent from "./demoWrap";
+import WrappedStorageComponent from "./wrapperStorage";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <WrappedComponent />
+        {/* <WrappedStorageComponent reallyLongApiCall={reallyLongApiCall} /> */}
       </header>
     </div>
   );
